f1015f3dec433e649e9cc841b9c35d37266c09c6
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-07-08 21:43:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ class BoardMember {
     return 'You will see great things from Scuber.'
   }
 
-  releasePressStatement() {
-    return 'You will see great things from Scuber.'
+  sayHi() {
+    return 'Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.'
   }
 }
